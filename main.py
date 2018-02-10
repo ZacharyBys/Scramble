9441b81c54a6676853871b8fe7fe478483166f8b
ZacharyBys <zachary.bys@gmail.com>
2018-02-10 15:08:53 -0500

    Supports multiple variable scrambling with random generated strings

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
+import string
+import random
+def randomString(size=8, chars=string.ascii_uppercase):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 inFile = open("samplecode.txt", "r")
 codeLines = inFile.readlines()
 variables = []
+replacementVariables = []
 newLines = []
+
 for line in codeLines:
+    currLine = line
     line = line.split()
-    currLine = ""
     n = 0
     for word in line:
         if word == "var":
             variables.append(line[n+1])
-        elif word == variables[0]:
-            word = "SCRAMBLED"
-        currLine += word + " "
+            replacementVariables.append(randomString())
         n += 1
+    for index, var in enumerate(variables):
+        currLine = currLine.replace(var, replacementVariables[index])
+
+      
     newLines.append(currLine)
 
 for line in newLines:
     print(line)
+
+
 
