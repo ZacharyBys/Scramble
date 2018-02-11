da121f2e62166aee684e79ae9be85585a818c83d
ZacharyBys <zachary.bys@gmail.com>
2018-02-10 19:09:04 -0500

    Squishing random lines of code together

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ variables = []
 replacementVariables = []
 newLines = []
 
+# Replaces variables with randomly generated strings
+################################################################################
 for line in codeLines:
     currLine = line
     line = line.split()
@@ -22,11 +24,27 @@ for line in codeLines:
         n += 1
     for index, var in enumerate(variables):
         currLine = currLine.replace(var, replacementVariables[index])
-
-      
+     
     newLines.append(currLine)
+################################################################################
 
-outFile.writelines(newLines)
+
+# Squishes Lines of code together randomly
+################################################################################
+scrambledLines = []
+x=0
+while (x < len(newLines)) :
+    addedLine = newLines[x]
+    while (random.randint(1,2) == 1):
+        x += 1
+        if (x < len(newLines)):
+            addedLine += newLines[x].strip()
+    
+    x += 1
+    scrambledLines.append(addedLine)  
+################################################################################
+
+outFile.writelines(scrambledLines)
 
 inFile.close()
 outFile.close()
